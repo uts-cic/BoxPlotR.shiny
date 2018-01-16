a99327bbb2ad26b77244cf71721eeef27501bbc2
sjgknight <sjgknight@gmail.com>
2018-01-16 15:00:09 +1100

    Update server.R

--- server.R
+++ server.R
@@ -42,7 +42,7 @@ dataM <- reactive({
 			data <- data.frame(gs_read(gs_data, ws = input$gsheetws))
 			
 			var <- input$ggrouping
-			data <- dcast(melt(data, id.vars=c(input$ggrouping), measure.vars=c(input$gdataID)), as.formula(paste0("value~",var))) #NOTE replace 'region' with a variable, and '3' with a range variable from input
+			data <- dcast(melt(data, id.vars=c(input$ggrouping), measure.vars=c(input$gdataID)), value~Region) #NOTE replace 'region' with a variable, and '3' with a range variable from input
 			
 			n <- ncol(data)
 			data <- data()[,-c(1,n)]
