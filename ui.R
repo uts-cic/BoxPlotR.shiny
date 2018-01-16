51b96250f0001ab17c3f5c0c2623b7f3ddd48020
sjgknight <sjgknight@gmail.com>
2018-01-16 11:52:46 +1100

    Update ui.R

--- ui.R
+++ ui.R
@@ -44,7 +44,7 @@ shinyUI(pageWithSidebar(
 				h5("Do you have a google sheet with data in it?"),
 				textInput("gsheetURL", "Google Sheet URL", value = "", width = '80%'),
 				textInput("gsheetws", "Google Sheet worksheet name", value = "", width = '80%'),
-				submitButton(text = "Apply Changes", icon = NULL, width = NULL)
+				submitButton(text = "Apply Changes", icon = ("refresh"), width = '80%')
 			)
 ),
 		conditionalPanel(condition="input.tabs1=='Data visualization'",
