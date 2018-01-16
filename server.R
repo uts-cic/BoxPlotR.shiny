56d7080416cc5878700bc8113ef5c17e5acb2fd7
sjgknight <sjgknight@gmail.com>
2018-01-16 14:27:00 +1100

    Update server.R

--- server.R
+++ server.R
@@ -39,11 +39,12 @@ dataM <- reactive({
 			gs_data <- gs_url(input$gsheetURL)
 
 			data <- data.frame(gs_read(gs_data, ws = "US_literacy"))
-			data() <- data.frame(gs_read(gs_data, ws = textInput$gsheetws))
+			data <- data.frame(gs_read(gs_data, ws = textInput$gsheetws))
 			
-			data <- dcast(melt(data(), id.vars=c("Region"), measure.vars=c(3)), value~Region) #NOTE replace 'region' with a variable, and '3' with a range variable from input
-			n <- ncol(data())
-			data <- data()[,-c(1,n)]
+			data <- dcast(melt(data, id.vars=c("Region"), measure.vars=c(3)), value~Region) #NOTE replace 'region' with a variable, and '3' with a range variable from input
+			n <- ncol(data)
+			data <- data[,-c(1,n)]
+			print(data)
 			#data <- data.frame(write.table(gs_read(gs_data, ws = "US_literacy")))
 			#data <- data.frame(na.omit(gs_read(input$gsheetURL, ws = input$gsheetws)))
 			if (is.null(input$gsheetURL)) {return(NULL)}
